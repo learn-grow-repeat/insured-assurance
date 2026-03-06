2ac8bd8a67444d747c3113691970f763befc9472
SwathyMenon <swathymenon.316@gmail.com>
2026-03-06 18:23:49 +0000

    changed to war

--- src/main/java/com/insuredassurance/InsuredAssuranceApplication.java
+++ src/main/java/com/insuredassurance/InsuredAssuranceApplication.java
@@ -2,9 +2,17 @@ package com.insuredassurance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InsuredAssuranceApplication {
+public class InsuredAssuranceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InsuredAssuranceApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(InsuredAssuranceApplication.class, args);
     }
